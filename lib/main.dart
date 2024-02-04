ceb065a5bde6f52306c32024efe1632c8117aaf8
mohdi <mohdinzamam12125@gmail.com>
2024-02-04 22:25:45 +0530

    title changing

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Covid 19 Tracker',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         brightness: Brightness.dark,
